7abc033f38d101ba2c2983a769c51c491f104d7c
Hanna Huewe <hanna.huewe@verdigado.com>
2023-10-17 15:00:27 +0200

    add initial css from the ci 2017

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'sunflower_child_enqueue_styles' );
+function sunflower_child_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( 'sunflower-style' ), 
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
+}
